ce32f2e40f8358fb43374d5a11a4586d3ed80e84
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-10-02 08:43:17 +0300

    Feature async migrations * fixture is now creating async engine instead of connection * await engine is disposed when test process tear down * await migrate process * db select query now creates new connection

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -7,11 +7,9 @@ from models import User
 
 
 @pytest.mark.asyncio
-async def test_migrations_run(db_connection):
-    connection, engine = db_connection
+async def test_migrations_run(engine):
     async_session = sessionmaker(engine, expire_on_commit=False, autoflush=False, class_=AsyncSession)
 
-    conn = await connection
     user_db = User(email="user@example.com", password="password", is_active=True)
     async with async_session(bind=engine) as session:
         # add user
@@ -20,11 +18,10 @@ async def test_migrations_run(db_connection):
         await session.refresh(user_db)
         assert user_db.id == 1
 
+    async with engine.connect() as conn:
         query = select(User).where(User.email == "user@example.com")
         res = await conn.execute(query)
-
         user = res.fetchone()
-
         assert user.id == 1
 
 
