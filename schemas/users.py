25d0544dac33da9b2e5dad6b44f66a465e14a25d
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-10-03 13:46:46 +0300

    Feature Users schemas: * created module schemas * installed pydantic email-validator and its dependencies * created pydantic schemas (UserBase, UserCreate, UserUpdate, UserDB) * added test for all schemas

---
+++ schemas/users.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+# base shared properties
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    confirmed: bool = False
+
+
+# user create query
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+# update user query
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+# user attributes stored in db
+class UserDB(UserBase):
+    id: Optional[int] = None
+    password: str
+    created: Optional[datetime] = None
+    last_login: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+
