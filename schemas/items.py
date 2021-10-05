3a1abf1760d2090f9efbf2b562ea583d14a77eed
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-10-05 09:26:52 +0300

    Refactor directory structure * introduced new type APIRouter * moved Items to schemas * moved create_item and get_items to views * moved root to welcome.py to views * connected all views via APIRouter

---
+++ schemas/items.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Item(BaseModel):
+    name: str = Field(..., title="Item name", example="Banana")
+    description: Optional[str] = Field(None, title="Item description", example="One pound of banana")
+    price: float = Field(..., title="Price of item without tax", example=4.99)
+    tax: Optional[float] = Field(None, title="Tax amount", example="null")
